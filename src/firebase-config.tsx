a43608331c67290bf9aed3b8857cc84c90748b59
taiwo <taiwooyindamola515@gmail.com>
2021-12-18 21:34:19 +0100

    added functionalities

--- src/firebase-config.tsx
+++ src/firebase-config.tsx
@@ -1,13 +1,7 @@
 import  {initializeApp } from "firebase/app"
 import { getFirestore } from "@firebase/firestore"
 const firebaseConfig = {
-    apiKey: "AIzaSyCKOnMeLTdTI99HZ4k6kA83EcWLdTBNMLs",
-    authDomain: "clone-efd4a.firebaseapp.com",
-    projectId: "clone-efd4a",
-    storageBucket: "clone-efd4a.appspot.com",
-    messagingSenderId: "704300523499",
-    appId: "1:704300523499:web:d0 edf99cde4480b12403ae",
-    measurementId: "G-LTB7FSN2KX"
+  
   };
 
   // Initialize Firebase
--- src/firebase.ts
+++ src/firebase.ts
@@ -2,18 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCKOnMeLTdTI99HZ4k6kA83EcWLdTBNMLs",
-    authDomain: "clone-efd4a.firebaseapp.com",
-    projectId: "clone-efd4a",
-    storageBucket: "clone-efd4a.appspot.com",
-    messagingSenderId: "704300523499",
-    appId: "1:704300523499:web:d0 edf99cde4480b12403ae",
-    measurementId: "G-LTB7FSN2KX"
+   
   };
 
  
 firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage().ref(`/images/cover.jpg`)
+const storage = firebase.storage()
 
 export default storage;
 
